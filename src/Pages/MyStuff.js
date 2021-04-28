d40ded3e9b7e5c44f7f2a080389796c623add612
Bryson Goins <brysonmg@gmail.com>
2021-04-28 16:29:25 -0500

    my groups list working

--- src/Pages/MyStuff.js
+++ src/Pages/MyStuff.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { MyCollection } from '../Components/MyCollection'
+import { MyGroups } from '../Components/MyGroups'
 
 export const MyStuff = () => {
     return (
         <>
             <MyCollection />
+            <MyGroups />
         </>
     )
 }
