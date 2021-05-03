582b1b5cedd01e37afe77968863ee666efabadbe
Bryson Goins <brysonmg@gmail.com>
2021-05-03 13:53:11 -0500

    group chat hidden if not member

--- src/Pages/IndividualGroup.js
+++ src/Pages/IndividualGroup.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { GroupChatBox } from '../Components/GroupChatBox'
 import {IndivGroupDetails} from '../Components/IndivGroupDetails'
 import {IndivGroupMembersList} from '../Components/IndivGroupMembersList'
+import {useParams} from 'react-router-dom'
 
 export const IndividualGroup =()=> {
     const [toggle, setToggle] = useState(true)
+    const [member, setMember] = useState(false)
 
     const toggleFunction = () => {
         if (toggle) {
@@ -14,11 +16,30 @@ export const IndividualGroup =()=> {
         }
     }
 
+    const currentUserId = parseInt(sessionStorage.getItem("nexusUser"))
+    const {groupId} = useParams()
+
+    const checkIfUserIsMember = () => {
+        fetch(`http://localhost:8088/groupMembers?userId=${currentUserId}&groupId=${groupId}`)
+        .then(res=>res.json())
+        .then(memberCheck => {
+            if (memberCheck.length > 0) {
+                setMember(true)
+            } else {
+                setMember(false)
+            }
+        })
+    }
+
+    useEffect(()=> {
+        checkIfUserIsMember()
+    })
+
     return (
         <>
             <IndivGroupDetails toggle={toggleFunction} />
             <IndivGroupMembersList toggle={toggle} />
-            <GroupChatBox />
+            {member ? <GroupChatBox /> : null}
         </>
     )
 }
